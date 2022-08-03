4c5afbfb6729da458a8ed69a5818e4a1748af48f
unknown <81542046+Briley0205@users.noreply.github.com>
2022-08-03 15:35:57 +0900

    restart it

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#FFDFDE",
+  textColor: "black",
+  accentColor: "#6A7BA2",
+  //cardBgColor: "transparent",
+};
