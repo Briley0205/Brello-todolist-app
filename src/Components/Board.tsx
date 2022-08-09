f5adf21e575435ea33b280a6dcf3730cc802e548
unknown <81542046+Briley0205@users.noreply.github.com>
2022-08-09 15:14:42 +0900

    change colors

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -26,9 +26,14 @@ interface IDropAreaProps {
 }
 const DropArea = styled.div<IDropAreaProps>`
   background-color: ${(props) =>
-    props.isDraggingOver ? "pink" : props.draggingFromThis ? "red" : "blue"};
+    props.isDraggingOver
+      ? "#dfe6e9"
+      : props.draggingFromThis
+      ? "#b2bec3"
+      : "transparent"};
   flex-grow: 1;
   transition: background-color 0.2s ease-in-out;
+  padding: 20px;
 `;
 
 interface IBoardProps {
